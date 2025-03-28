72f42c3c7447cad6ec57f627136bb9edd6401350
krisawyerr <krisawyerrr@gmail.com>
2025-03-28 13:15:36 -0700

    updated server and added env example

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use("/api/homes", homesRoutes)
 app.use("/api/auth", authRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
